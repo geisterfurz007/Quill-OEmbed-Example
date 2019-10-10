c1506c98b2a3d1c03c0aad828f1b03dc3e6974f2
geisterfurz007 <michelvonv@me.com>
2019-10-10 09:43:19 +0200

    Add module in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,23 @@ import React from 'react';
 import './App.css';
 
 import { RichTextEditor } from "@sensenet/controls-react";
+import QuillOEmbedModule from './QuillOEmbedModule';
 
+import { Quill } from 'react-quill';
+
+Quill.register('modules/oembed', QuillOEmbedModule);
 
 const App: React.FC = () => {
 	return (
 		<div className="App">
 			<RichTextEditor settings={{
-				Name: 'James',
-				DisplayName: 'Jamie',
+				Name: 'Some name',
+				DisplayName: 'Some Displayname',
 				Compulsory: true,
 				ReadOnly: false,
-				DefaultValue: 'Dis some text',
-				Type: 'THE! Type',
-				FieldClassName: 'johnny'
+				DefaultValue: 'Some text',
+				Type: 'Some Type',
+				FieldClassName: 'some-class-name'
 			}} actionName={'edit'}/>
 
 		</div>
